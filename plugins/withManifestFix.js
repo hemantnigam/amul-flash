cb43bac2745cd42bc66bb0e6eaf5b0b950a6cf14
Hemant Nigam <hnigam@chegg.com>
2026-08-30 12:42:00 +0530

    fix(build): remove conflicting notification color and force tools:replace in manifest

--- plugins/withManifestFix.js
+++ plugins/withManifestFix.js
@@ -5,24 +5,35 @@ module.exports = function withManifestFix(config) {
     const androidManifest = config.modResults.manifest;
     const application = androidManifest.application?.[0];
 
-    // Ensure tools namespace is added to AndroidManifest
+    // Ensure tools namespace is added to AndroidManifest root
     if (!androidManifest.$) {
       androidManifest.$ = {};
     }
-    if (!androidManifest.$['xmlns:tools']) {
-      androidManifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
-    }
+    androidManifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
 
     if (application && Array.isArray(application['meta-data'])) {
-      for (const metaData of application['meta-data']) {
+      // 1. Tag all firebase notification meta-data with tools:replace
+      application['meta-data'].forEach((metaData) => {
         const name = metaData.$?.['android:name'];
         if (
           name === 'com.google.firebase.messaging.default_notification_color' ||
           name === 'com.google.firebase.messaging.default_notification_icon'
         ) {
+          if (!metaData.$) metaData.$ = {};
           metaData.$['tools:replace'] = 'android:resource';
         }
-      }
+      });
+
+      // 2. Remove duplicate default_notification_color entries if multiple exist
+      let seenColor = false;
+      application['meta-data'] = application['meta-data'].filter((metaData) => {
+        const name = metaData.$?.['android:name'];
+        if (name === 'com.google.firebase.messaging.default_notification_color') {
+          if (seenColor) return false;
+          seenColor = true;
+        }
+        return true;
+      });
     }
 
     return config;
